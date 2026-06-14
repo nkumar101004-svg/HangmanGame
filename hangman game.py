dcf5c7c46d2925b4a43e8e9ee05eb04940be8f24
nkumar101004-svg <nkumar101004@gmail.com>
2026-06-14 22:51:36 +0530

    Add files via upload

---
+++ hangman game.py
@@ -0,0 +1,35 @@
+import random
+
+words = ["apple", "python", "mango", "house", "chair"]
+word = random.choice(words)
+
+guessed = []
+chances = 6
+
+while chances > 0:
+    display = ""
+
+    for letter in word:
+        if letter in guessed:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word:", display)
+
+    if display == word:
+        print("You Win!")
+        break
+
+    guess = input("Enter a letter: ")
+
+    if guess in word:
+        guessed.append(guess)
+        print("Correct!")
+    else:
+        chances -= 1
+        print("Wrong! Chances left:", chances)
+
+if chances == 0:
+    print("Game Over!")
+    print("The word was:", word)
